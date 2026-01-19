b0884c02196633cf29ba36a6c892c6505b818ca3
Danlois Tovar <danlois.tovar@gmail.com>
2026-01-19 14:34:32 +0100

    chore(build): update generated files after merge

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  adapter: vercel(),
+  adapter: vercel(), // Descomenta cuando depliegues en Vercel
   vite: {
     plugins: [tailwindcss()],
   },
